9856fe787f2bfbe5a868b286cedbb4ecd48db9e1
tenmo2003 <tenmo2003@gmail.com>
2023-09-12 14:13:49 +0700

    lab thuc don finished

--- Lab. Thuc don/script.js
+++ Lab. Thuc don/script.js
@@ -1,13 +1,25 @@
 var table = document.getElementById("menu");
-
 var tableData = table.getElementsByTagName("td");
-
-var selected = null;
+var selectedRow = null;
 
 function selected(data) {
     for (var i = 0; i < tableData.length; i++) {
-        tableData[i].className = "normal";
+        tableData[i].classList.remove("active");
     }
 
-    data.className = "active";
+    data.classList.add("active");
+    selectedRow = data.innerText;
+    updateSelectedInfo();
+}
+
+function updateSelectedInfo() {
+    var selectedInfo = document.getElementById("selected-info");
+    selectedInfo.textContent = (selectedRow || "None");
+}
+
+// Attach the click event to the table rows
+for (var i = 0; i < tableData.length; i++) {
+    tableData[i].addEventListener("click", function () {
+        selected(this);
+    });
 }
